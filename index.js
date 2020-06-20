fdfb0afc37f294ed2c071566044e45c2d9d0aea5
Alvaro Obyrne <alvarobyrne@gmail.com>
2020-06-20 14:13:24 -0500

    big time editors

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var chokidar = require('chokidar');
+chokidar.watch(['index.js','index.html'],{}).on('change',(a,b)=>{
+    console.log("a : ",a);
+    console.log("b : ",b);
+    location.reload();
+})
+
 var get_source = function(main_dir, dir, file){
         var disclaimer = "";
         if(file != "Facade")
@@ -7,11 +14,20 @@ var get_source = function(main_dir, dir, file){
             url: url,
             dataType: "script",
             success: function(data){
-                var editor = ace.edit("editor_"+file);
+                let ace_file = "editor_"+file;
+                console.log("ace_file : ",ace_file);
+                var editor = ace.edit(ace_file);
+                editor.on('change', (arg, activeEditor) => {
+                  const aceEditor = activeEditor;
+                  const newHeight = aceEditor.getSession().getScreenLength() *
+                    (aceEditor.renderer.lineHeight + aceEditor.renderer.scrollBar.getWidth());
+                   aceEditor.container.style.height = `${newHeight}px`;
+                  aceEditor.resize();
+                });
                 editor.setTheme("ace/theme/chrome");
                 editor.getSession().setMode("ace/mode/javascript");
-                editor.getSession().setUseWrapMode(true);
-                editor.setReadOnly(true);
+                // editor.getSession().setUseWrapMode(true);
+                // editor.setReadOnly(true);
                 editor.setValue(disclaimer+data,-1);
             },
             error: function(data){
